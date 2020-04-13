6833c94fdd4e58351a27a965e525503d545f1778
Brandon Hernández <brandonhernandez_10@outlook.es>
2020-04-13 09:33:48 -0600

    Initial commit

---
+++ Assets/Code/Player.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public float force = 2f;
+    public float jumpForce = 5f;
+    
+    public Rigidbody rgb;
+    int powerUp = 0;
+
+    public Text score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rgb = GetComponent<Rigidbody>();
+
+        score = GameObject.FindGameObjectWithTag("ScoreText").GetComponent<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rgb && Input.GetButtonDown("Jump") && (Time.timeScale > 0.0f))
+        {
+            Jump(); 
+        }
+
+        if (score)
+        {
+            score.text = "Score: " + powerUp.ToString() + "/3";
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (rgb)
+        {
+            rgb.AddForce(Input.GetAxis("Horizontal") * force, 0,
+                Input.GetAxis("Vertical") * force);
+        }
+    }
+    private void Jump()
+    {
+        if (Mathf.Abs(rgb.velocity.y) < 0.05f)
+            rgb.AddForce(0, jumpForce, 0, ForceMode.Impulse);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && powerUp != 3)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PowerUp"))
+        {
+            powerUp++;
+            Destroy(other.gameObject);
+        }
+            
+    }
+}
---
+++ Assets/Code/Spawn.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Spawn : MonoBehaviour
+{
+
+    // Instanciar
+    public GameObject prefab;
+    private GameObject newObj;
+
+    Vector3 startPosition;
+
+    public GameObject pauseMenu;
+    private bool active = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1.0f;
+        startPosition = gameObject.transform.position;
+        newObj = Instantiate(prefab, startPosition, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Return) && !newObj)
+        {
+            newObj = Instantiate(prefab, startPosition, Quaternion.identity);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+        }
+    }
+
+    public void TogglePause()
+    {
+        if (pauseMenu)
+        {
+            pauseMenu.SetActive(!pauseMenu.activeSelf);
+            active = !active;
+            Time.timeScale = active ? 0.0f : 1.0f;
+        }
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void ChangeScene(int indexScene)
+    {
+        SceneManager.LoadScene(indexScene);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
